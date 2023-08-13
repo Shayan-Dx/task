b1be31c8c21d470d39027524059854da6006ddd1
Shayan_Dx <shayanvafi0@gmail.com>
2023-08-13 11:21:16 +0330

    serializers has been added.

---
+++ store_api/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+
+from .models import Category, Product
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ('id', 'title', 'description')
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    categories = CategorySerializer(many=True)
+
+    class Meta:
+        model = Product
+        fields = ('id', 'title', 'description', 'categories')
