a9dd1bc963090e372faf797921961064342ef10f
Shayan_Dx <shayanvafi0@gmail.com>
2023-08-13 10:07:53 +0330

    models for categories and products has been added.

--- store_api/models.py
+++ store_api/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    parent = models.ForeignKey('self', verbose_name='parent', blank=True, null=True, on_delete=models.CASCADE)
+    title = models.CharField('title', max_length=20)
+    description = models.TextField('description', blank=True)
+    is_enable = models.BooleanField('is enable', default=True)
+    created_time = models.DateTimeField('created time', auto_now_add=True)
+    updated_time = models.DateTimeField('updated time', auto_now=True)
+
+    class Meta:
+        db_table = 'categories'
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.title
+    
+
+class Product(models.Model):
+    title = models.CharField('title', max_length=20)
+    description = models.TextField('description', blank=True)
+    is_enable = models.BooleanField('is enable', default=True)
+    categories = models.ManyToManyField('Category', verbose_name='categories', blank=True)
+    created_time = models.DateTimeField('created time', auto_now_add=True)
+    updated_time = models.DateTimeField('updated time', auto_now=True)
+
+    class Meta:
+        db_table = 'products'
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
+
+    def __str__(self):
+        return self.title
