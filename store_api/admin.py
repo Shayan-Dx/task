1b1055a99251f32ce15965db3786a72b45224721
Shayan_Dx <shayanvafi0@gmail.com>
2023-08-13 12:10:05 +0330

    added the categories view and update functionality.

--- store_api/admin.py
+++ store_api/admin.py
@@ -4,14 +4,14 @@ from .models import Product, Category
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ["title", "is_enable", "created_time"]
+    list_display = ["id", "title", "is_enable", "created_time"]
     list_filter = ["is_enable"]
     search_fields = ["title"]
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ["title", "is_enable", "created_time"]
+    list_display = ["id", "title", "is_enable", "created_time"]
     list_filter = ["is_enable"]
     search_fields = ["title"]
     filter_horizontal = ['categories']
