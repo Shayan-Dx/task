01a218779db0b0fd0e475ca331be3f890c91789f
Shayan_Dx <shayanvafi0@gmail.com>
2023-08-13 11:37:11 +0330

    added the list and detail product view

--- store_api/views.py
+++ store_api/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Category, Product
+from .serializers import CategorySerializer, ProductSerializer
+
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+
+
+class ProductListView(APIView):
+    def get(self, request):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True, context={'request':request})
+        return Response(serializer.data)
+    
+
+class ProductDetailView(APIView):
+    def get(self, request, primary):
+        try:
+            product = Product.objects.get(pk=primary)
+        except Product.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = ProductSerializer(product, context={'request':request})
+        return Response(serializer.data)
